4286bb717d915e7ccd6ec3b2fddca046f1630e88
Arda Doğru <ardadr95@gmail.com>
2025-07-01 16:59:17 +0300

    Library Refactored

--- airbnb_model.py
+++ airbnb_model.py
@@ -2,7 +2,6 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-from matplotlib.lines import lineStyles
 
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
