4d542e88b3ddedc5a238d016358b658131310f1d
Arda Doğru <ardadr95@gmail.com>
2025-07-01 12:30:41 +0300

    Added (Log Transform and Disable Outlier Values)

--- airbnb_model.py
+++ airbnb_model.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
@@ -6,7 +7,7 @@ from sklearn.metrics import mean_squared_error, r2_score
 df = pd.read_csv("AB_NYC_2019.csv")
 print(df.head())
 
-# Introduction about dataset
+# Information about dataset
 print(df.info())
 
 # Missing data check
@@ -18,6 +19,13 @@ print('\nPrice distribution:\n', df["price"].describe())
 # Remove unnecessary columns for price prediction
 df = df.drop(["id", "name","host_name","last_review","neighbourhood"], axis=1)
 df = df.dropna()
+
+# Disable outlier values
+df = df[df["price"] > 0]
+df = df[df["price"] < 500]
+
+# Make log transform
+df["price"] = np.log(df["price"])
 
 print("New data size", df.shape)
 
@@ -46,4 +54,10 @@ y_pred = model.predict(X_test)
 print("Mean Squared Error", mean_squared_error(y_test,y_pred))
 print("R2 Score:", r2_score(y_test,y_pred))
 
+# Transform log values to real values
+actual_prices = np.exp(y_test)
+predicted_prices = np.exp(y_pred)
+
+for real,pred in list(zip(actual_prices, predicted_prices))[:10]:
+    print(f"Actual Price: {real:.2f} USD, Predict: {pred: .2f} USD")
 
